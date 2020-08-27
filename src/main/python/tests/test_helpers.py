ac1dc3f78bf9d05183364f7f10e5af6b41fb9832
Till Schröder <45294825+Starbat@users.noreply.github.com>
2020-08-27 11:28:48 +0200

    add test_helpers

---
+++ src/main/python/tests/test_helpers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pytest
+from transformer.summary_extractor import SummaryExtractor
+from transformer.measurement_extractor import MeasurementExtractor
+from transformer.helpers import get_extractors
+
+
+def test_get_summary_extractor():
+    output = get_extractors(summaries=True)
+    assert type(output[0]) == SummaryExtractor
+    assert len(output) == 1
+
+
+
+def test_get_measurement_extractor():
+    output = get_extractors(measurements=True)
+    assert type(output[0]) == MeasurementExtractor
+    assert len(output) == 1
+
+
+def test_get_two_extractors():
+    output = get_extractors(measurements=True, summaries=True)
+    assert len(output) == 2
