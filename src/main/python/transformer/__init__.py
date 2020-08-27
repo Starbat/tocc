a1a61120d37c17f3dbc1cd4567d5e72f4e41bb7a
Till Schröder <45294825+Starbat@users.noreply.github.com>
2020-08-27 15:43:54 +0200

    fix import

--- src/main/python/transformer/__init__.py
+++ src/main/python/transformer/__init__.py
@@ -1,3 +1,4 @@
 from .table_transformer import TableTransformer
 from .summary_extractor import SummaryExtractor
 from .measurement_extractor import MeasurementExtractor
+from .helpers import get_extractors
